6df82c17774f21c2886d75a30773cb82d1f4a130
Vinícius Costa <96350207+Vinicius-C13@users.noreply.github.com>
2023-08-09 15:04:24 -0300

    script para a conversão de binário para decimal finalizado.

--- bin2dec/script.js
+++ bin2dec/script.js
@@ -3,7 +3,19 @@ const form = document.querySelector("form");
 const input = document.querySelector("#input");
 const output = document.querySelector("#output");
 
+function bin2dec(value) {
+  let decimal = 0;
+  for (let i = 0; i < value.length; i++) {
+    if (Number(value[value.length - 1 - i]) == 0) {
+      continue;
+    } else {
+      decimal = decimal + 2 ** i;
+    }
+  }
+  return decimal;
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log(input.value);
+  output.value = bin2dec(input.value);
 });
